75df4b069b38346bb44eaec0cadb932bf8d96eff
jobsodev1 <jam.meloalves@gmail.com>
2021-01-01 12:06:11 -0300

    inicio

---
+++ src/database/migrations/1609512740385-CreateUsuario.ts
@@ -0,0 +1,12 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export default class CreateUsuario1609512740385 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
