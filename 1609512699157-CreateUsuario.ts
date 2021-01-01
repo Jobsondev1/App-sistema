499a6bc02488e47946d67c6ecff3bdb2c89dab49
jobsodev1 <jam.meloalves@gmail.com>
2021-01-01 12:11:05 -0300

    migrations funcionando

---
+++ 1609512699157-CreateUsuario.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsuario1609512699157 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
